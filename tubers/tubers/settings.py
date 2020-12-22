dbdbc7f56a84f419186bcd3176423cc7a3203cdc
sudharsan004 <sudharsansriram8921@gmail.com>
2020-12-22 20:01:33 +0530

    setup for heroku2

--- tubers/tubers/settings.py
+++ tubers/tubers/settings.py
@@ -80,11 +80,8 @@ WSGI_APPLICATION = 'tubers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lcotubers',
-        'USER': 'postgres',
-        'PASSWORD': 'pass',
-        'HOST': '127.0.0.1'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
