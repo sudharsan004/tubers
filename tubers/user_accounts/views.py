e145a305c576845af17f8eddba42b5c4b6e4d28e
sudharsan004 <sudharsansriram8921@gmail.com>
2020-12-24 12:02:30 +0530

    create templates

--- tubers/user_accounts/views.py
+++ tubers/user_accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login(request):
+    pass
+
+
+# Django already has a build in logout method so give some other name to logout function
+def logout_user(request):
+    pass
+
+
+def register(request):
+    pass
+
+
+def dashboard(request):
+    pass
