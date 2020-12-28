ca952a8bb0b77d814114ace559ba4e2c0ba65462
sudharsan004 <sudharsansriram8921@gmail.com>
2020-12-28 21:12:36 +0530

    create contact_info app

---
+++ tubers/contact_info/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+# Create your models here.
+
+
+class ContactInfo(models.Model):
+    email = models.EmailField()
+    phone = PhoneNumberField()
+    facebook_link = models.URLField()
+    twitter_link = models.URLField()
+    instagram_link = models.URLField()
+    youtube_link = models.URLField()
