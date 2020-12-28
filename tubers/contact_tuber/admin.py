474f0a3d63f281b5aa3e8949ab159de3c1ff88ca
sudharsan004 <sudharsansriram8921@gmail.com>
2020-12-28 12:21:11 +0530

    update admin panel  elements of contact tuber

--- tubers/contact_tuber/admin.py
+++ tubers/contact_tuber/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(ContactInfo)
+
+
+class ContactTuberAdmin(admin.ModelAdmin):
+    def youtuber_name(self, object):
+        return object.ytuber_contacted.name
+    list_display = ('first_name', 'email', 'youtuber_name')
+
+
+admin.site.register(ContactInfo, ContactTuberAdmin)
